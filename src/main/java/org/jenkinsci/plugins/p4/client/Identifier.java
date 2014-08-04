7b67457c31ffc40c71cbf835fbe39819f2c0eade
Paul Allen <pallen@perforce.com>
2014-08-04 15:22:46 +0100

    Report the plugin version.
    Extracts the plugin version and adds a name for reporting in the P4D
    logs. e.g.  [Jenkins_p4-plugin/1.0.10-SNAPSHOT]

--- src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
+++ src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
@@ -1,23 +1,26 @@
 package org.jenkinsci.plugins.p4.client;
 
+import hudson.PluginWrapper;
+import jenkins.model.Jenkins;
+
 public class Identifier {
 	private String product;
 	private String version;
-	
+
 	public Identifier() {
-		Package p = this.getClass().getPackage();
-		version = p.getSpecificationVersion();
-		if(version == null)
-			version = "UNSET";
-		product = p.getImplementationTitle();
-		if(product == null)
-			product = "Jenkins p4-plugin";
+		Jenkins jenkins = Jenkins.getInstance();
+		PluginWrapper plugin = jenkins.getPluginManager().getPlugin("p4");
+		String string = plugin.getVersion();
+		String[] parts = string.split(" ");
+
+		version = parts[0];
+		product = "Jenkins_p4-plugin";
 	}
-	
+
 	public String getVersion() {
 		return version;
 	}
-	
+
 	public String getProduct() {
 		return product;
 	}
