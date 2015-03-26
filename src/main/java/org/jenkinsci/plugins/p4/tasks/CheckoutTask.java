614ee95e83b6978c9f211c7685cab24cda03843f
Nick Palmer <nick@palmr.co.uk>
2015-03-26 16:24:08 +0000

    Use getPin() from populate config on task when getting the BuildChange object to sync

--- src/main/java/org/jenkinsci/plugins/p4/tasks/CheckoutTask.java
+++ src/main/java/org/jenkinsci/plugins/p4/tasks/CheckoutTask.java
@@ -158,6 +158,19 @@ public class CheckoutTask extends AbstractTask implements
 			}
 		}
 
+		// if label is specified via label in populate config then update
+		String populateLabel = populate.getPin();
+		if (populateLabel != null && !populateLabel.isEmpty()) {
+			// Expand label with environment vars if one was defined
+			populateLabel = getWorkspace().expand(populateLabel, false);
+			try {
+				// if build is a change-number passed as a label
+				build = Integer.parseInt(populateLabel);
+			} catch (NumberFormatException e) {
+				build = populateLabel;
+			}
+		}
+
 		// if label is specified then update
 		String label = workspace.get(ReviewProp.LABEL.toString());
 		if (label != null && !label.isEmpty()) {
