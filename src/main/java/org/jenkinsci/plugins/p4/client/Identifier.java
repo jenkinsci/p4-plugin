3f6ed2af0016505cf580ac6efcdd42080210e1fa
Paul Allen <pallen@perforce.com>
2017-02-09 16:31:46 +0000

    Expand Version and ID in Server log.

--- src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
+++ src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
@@ -1,19 +1,28 @@
 package org.jenkinsci.plugins.p4.client;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 public class Identifier {
 	private String product;
 	private String version;
 
-	public Identifier() {
-		ResourceBundle bundle = ResourceBundle
-				.getBundle("org.jenkinsci.plugins.p4.Identifier");
+	public Identifier() throws IOException, XmlPullParserException {
+		ResourceBundle bundle = ResourceBundle.getBundle("org.jenkinsci.plugins.p4.Identifier");
 
-		version = bundle.getString("org.jenkinsci.plugins.p4.version");
-		product = bundle.getString("org.jenkinsci.plugins.p4.product");
+		MavenXpp3Reader reader = new MavenXpp3Reader();
+		Model model = reader.read(new FileReader("pom.xml"));
 		
+		version = model.getVersion();
+		product = model.getArtifactId();
+
 		String platform = System.getProperty("os.name");
+		platform = platform.replaceAll(" ", "_");
 		version += "/" + platform;
 	}
 
