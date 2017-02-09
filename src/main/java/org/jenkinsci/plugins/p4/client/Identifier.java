ff429b098ae34b8e68949cec517b755f2d5b2f9c
Paul Allen <pallen@perforce.com>
2017-02-09 17:37:48 +0000

    FindBug fixes for reading the POM.

--- src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
+++ src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
@@ -4,20 +4,18 @@ import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ResourceBundle;
+import java.io.InputStreamReader;
 
 public class Identifier {
 	private String product;
 	private String version;
 
 	public Identifier() throws IOException, XmlPullParserException {
-		ResourceBundle bundle = ResourceBundle.getBundle("org.jenkinsci.plugins.p4.Identifier");
-
 		MavenXpp3Reader reader = new MavenXpp3Reader();
-		Model model = reader.read(new FileReader("pom.xml"));
-		
+		Model model = reader.read(new InputStreamReader(new FileInputStream("pom.xml"), "UTF-8"));
+
 		version = model.getVersion();
 		product = model.getArtifactId();
 
