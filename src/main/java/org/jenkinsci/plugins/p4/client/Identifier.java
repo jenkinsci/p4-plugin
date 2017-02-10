23454db26a1433b323944a7929b8195cb86bd836
Paul Allen <pallen@perforce.com>
2017-02-10 12:24:58 +0000

    Identifier fix by adding filtering to resources.

--- src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
+++ src/main/java/org/jenkinsci/plugins/p4/client/Identifier.java
@@ -1,23 +1,16 @@
 package org.jenkinsci.plugins.p4.client;
 
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ResourceBundle;
 
 public class Identifier {
 	private String product;
 	private String version;
 
-	public Identifier() throws IOException, XmlPullParserException {
-		MavenXpp3Reader reader = new MavenXpp3Reader();
-		Model model = reader.read(new InputStreamReader(new FileInputStream("pom.xml"), "UTF-8"));
+	public Identifier() {
+		ResourceBundle bundle = ResourceBundle.getBundle("org.jenkinsci.plugins.p4.Identifier");
 
-		version = model.getVersion();
-		product = model.getArtifactId();
+		version = bundle.getString("org.jenkinsci.plugins.p4.version");
+		product = bundle.getString("org.jenkinsci.plugins.p4.product");
 
 		String platform = System.getProperty("os.name");
 		platform = platform.replaceAll(" ", "_");
