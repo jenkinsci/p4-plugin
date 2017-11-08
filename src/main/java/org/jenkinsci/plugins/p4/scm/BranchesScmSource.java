21a80559543dd2a267f6acea23bd125c6e744665
Fergus Byrne <fergus.byrne@murex.com>
2017-11-08 13:36:38 +0000

    Add the ability to provide a view spec to Helix Branches SCM

--- src/main/java/org/jenkinsci/plugins/p4/scm/BranchesScmSource.java
+++ src/main/java/org/jenkinsci/plugins/p4/scm/BranchesScmSource.java
@@ -1,12 +1,19 @@
 package org.jenkinsci.plugins.p4.scm;
 
+import com.google.common.collect.Iterables;
 import com.perforce.p4java.core.file.IFileSpec;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Extension;
 import hudson.model.TaskListener;
+import jenkins.scm.api.SCMSourceOwner;
 import org.jenkinsci.Symbol;
 import org.jenkinsci.plugins.p4.browsers.P4Browser;
 import org.jenkinsci.plugins.p4.client.ConnectionHelper;
+import org.jenkinsci.plugins.p4.workspace.ManualWorkspaceImpl;
+import org.jenkinsci.plugins.p4.workspace.Workspace;
+import org.jenkinsci.plugins.p4.workspace.WorkspaceSpec;
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory;
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
@@ -20,7 +27,8 @@ import java.util.regex.Pattern;
 public class BranchesScmSource extends AbstractP4ScmSource {
 
 	private P4Browser browser;
-  	private String filter;
+  private String filter = DescriptorImpl.defaultFilter;
+	private String mappings = DescriptorImpl.defaultPath;
 
 	@DataBoundConstructor
 	public BranchesScmSource(String id, String credential, String includes, String charset, String format) {
@@ -44,6 +52,13 @@ public class BranchesScmSource extends AbstractP4ScmSource {
 		return filter;
 	}
 
+	public String getMappings(){ return this.mappings; }
+
+	@DataBoundSetter
+	public void setMappings(String mappings) {
+		this.mappings = mappings;
+	}
+
 	@Override
 	public P4Browser getBrowser() {
 		return browser;
@@ -56,6 +71,7 @@ public class BranchesScmSource extends AbstractP4ScmSource {
 
 	@Override
 	public List<P4Head> getHeads(@NonNull TaskListener listener) throws Exception {
+
 		List<String> paths = getIncludePaths();
 		List<P4Head> list = new ArrayList<>();
 
@@ -97,9 +113,46 @@ public class BranchesScmSource extends AbstractP4ScmSource {
 		return list;
 	}
 
+	@Override
+	public Workspace getWorkspace(List<P4Path> paths) {
+		P4Path branchPath = Iterables.getFirst(paths, null);
+		if(branchPath == null){
+			throw new IllegalArgumentException("missing branch path");
+		}
+
+		String client = getFormat();
+		String mappingFormat = String.format("%1s/%%1$s //%2$s/%%1$s", branchPath.getPath(), client);
+
+		StringBuffer workspaceView = new StringBuffer(1024);
+		workspaceView.append(String.format(mappingFormat, getScriptPathOrDefault("Jenkinsfile")));
+		for(String mapping : getViewMappings()){
+			workspaceView.append("\n").append(String.format(mappingFormat, mapping));
+		}
+		WorkspaceSpec spec = new WorkspaceSpec(workspaceView.toString(), null);
+		return new ManualWorkspaceImpl(getCharset(), false, client, spec);
+	}
+
+	private String getScriptPathOrDefault(String defaultScriptPath) {
+		SCMSourceOwner owner = getOwner();
+		if(owner instanceof WorkflowMultiBranchProject){
+			WorkflowMultiBranchProject branchProject = (WorkflowMultiBranchProject) owner;
+			WorkflowBranchProjectFactory branchProjectFactory = (WorkflowBranchProjectFactory) branchProject.getProjectFactory();
+			return branchProjectFactory.getScriptPath();
+		}
+		return defaultScriptPath;
+	}
+
+	protected List<String> getViewMappings() {
+		return toLines(getMappings());
+	}
+
 	@Extension
 	@Symbol("multiBranch")
 	public static final class DescriptorImpl extends P4ScmSourceDescriptor {
+
+		public static final String defaultPath = "...";
+
+		public static final String defaultFilter = ".*";
 
 		@Override
 		public String getDisplayName() {
