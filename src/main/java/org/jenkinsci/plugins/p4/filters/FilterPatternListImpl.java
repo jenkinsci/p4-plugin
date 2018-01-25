f6de5c01d8a713fe7aaae8b3565e902251e4c0db
Jake Romigh <jromigh@gmail.com>
2018-01-24 18:13:02 -0800

    Adding more usability to Pattern Filter
    Adding some UI alerts when we detect bad entries (along with a new assert)

--- src/main/java/org/jenkinsci/plugins/p4/filters/FilterPatternListImpl.java
+++ src/main/java/org/jenkinsci/plugins/p4/filters/FilterPatternListImpl.java
@@ -1,8 +1,10 @@
 package org.jenkinsci.plugins.p4.filters;
 
 import hudson.Extension;
+import hudson.util.FormValidation;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -45,7 +47,7 @@ public class FilterPatternListImpl extends Filter implements Serializable {
 			try {
 				patternList.add(Pattern.compile(line, caseFlag));
 			}
-			catch(PatternSyntaxException e) {
+			catch (PatternSyntaxException e) {
 				logger.severe("Error processing supposed pattern \"" + line + "\", ignoring:\n" + e);
 			}
 		}
@@ -61,6 +63,24 @@ public class FilterPatternListImpl extends Filter implements Serializable {
 		public String getDisplayName() {
 			return "Exclude changes outside Java pattern";
 		}
-
+		
+		public FormValidation doCheckPatternList(@QueryParameter String value) {
+			if(value.trim().length() == 0) {
+				return FormValidation.warning("Empty pattern list found, will ignore all changes during polling.");
+			}
+			
+			int lineNumber = 1;
+			try {
+				for (String line: value.split("\n")) {
+					Pattern.compile(line);
+					lineNumber++;
+				}
+				return FormValidation.ok();
+			}
+			catch (PatternSyntaxException e) {
+				String errorMsg = "Error encountered compiling pattern on line #" + lineNumber + " (if not fixed, will ignore).";
+				return FormValidation.error(e, errorMsg);
+			}
+		}
 	}
 }
