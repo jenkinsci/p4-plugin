d9060181b495989b6290ce087705dd0a0ccabcb7
kroutley <kroutley@ea.com>
2018-09-04 13:02:33 -0700

    Add all FileAction enum types to P4AffectedFile

--- src/main/java/org/jenkinsci/plugins/p4/changes/P4AffectedFile.java
+++ src/main/java/org/jenkinsci/plugins/p4/changes/P4AffectedFile.java
@@ -43,17 +43,52 @@ public class P4AffectedFile implements AffectedFile {
 
 	private EditType parseFileAction(FileAction fileAction) {
 		switch (fileAction) {
+		case ABANDONED:
+			return new EditType("abandoned","The file was abandoned");
 		case ADD:
-		case MOVE_ADD:
+		case ADDED:
 			return EditType.ADD;
-
-		case EDIT:
-			return EditType.EDIT;
-
+		case BRANCH:
+			return new EditType("branch", "The file was branched");
+		case COPY_FROM:
+			return new EditType("copy_from", "The file was copied");
 		case DELETE:
-		case MOVE_DELETE:
+		case DELETED:
 			return EditType.DELETE;
-
+		case EDIT:
+		case EDIT_FROM:
+		case EDIT_IGNORED:
+			return EditType.EDIT;
+		case IGNORED:
+			return new EditType("ignored", "The file was ignored");
+		case IMPORT:
+			return new EditType("import", "The file was imported");
+		case INTEGRATE:
+			return new EditType("integrate", "The file was integrated");
+		case MERGE_FROM:
+			return new EditType("merge_from", "The file was merged");
+		case MOVE:
+			return new EditType("move", "The file was moved");
+		case MOVE_ADD:
+			return new EditType("move_add", "File was moved and added");
+		case MOVE_DELETE:
+			return new EditType("move_delete", "File was moved and deleted");
+		case PURGE:
+			return new EditType("purge", "The file was purged");
+		case REFRESHED:
+			return new EditType("refreshed", "The file was refreshed");
+		case REPLACED:
+			return new EditType("replaced", "The file was replaced");
+		case RESOLVED:
+			return new EditType("resolved", "The file was resolved");
+		case SYNC:
+			return new EditType("sync", "The file was synched");
+		case UNKNOWN:
+			return new EditType("unknown", "Unknown");
+		case UNRESOLVED:
+			return new EditType("unresolved", "The file was unresolved");
+		case UPDATED:
+			return new EditType("updated", "The file was updated");
 		default:
 			return EditType.EDIT;
 		}
