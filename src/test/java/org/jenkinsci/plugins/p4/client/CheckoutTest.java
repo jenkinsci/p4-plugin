4d049547b6292f134bc196bc8ea9cb938f0f72e6
Stuart Rowe <stuartrowe@gmail.com>
2019-05-22 15:52:15 -0700

    Fix whitespace in CheckoutTest to be consistent with other source files

--- src/test/java/org/jenkinsci/plugins/p4/client/CheckoutTest.java
+++ src/test/java/org/jenkinsci/plugins/p4/client/CheckoutTest.java
@@ -32,90 +32,90 @@ import java.util.logging.Logger;
 import static org.junit.Assert.assertEquals;
 
 public class CheckoutTest extends DefaultEnvironment {
-    
-    private static final Logger logger = Logger.getLogger(CheckoutTest.class.getName());
-    private static final String P4ROOT = "tmp-CheckoutTest-p4root";
-    
-    @Rule
-    public JenkinsRule jenkins = new JenkinsRule();
 
-    @Rule
-    public SampleServerRule p4d = new SampleServerRule(P4ROOT, R15_1);
+	private static final Logger logger = Logger.getLogger(CheckoutTest.class.getName());
+	private static final String P4ROOT = "tmp-CheckoutTest-p4root";
 
-    @Before
-    public void buildCredentials() throws Exception {
-	createCredentials("jenkins", "jenkins", p4d.getRshPort(), CREDENTIAL);
-    }
-    
-    @Test
-    public void testCheckoutUnrestrictedView() throws Exception {
-        String client = "CheckoutUnrestrictedView.ws";
-        String view = "//depot/... //" + client + "/...";
-	WorkspaceSpec spec = new WorkspaceSpec(view, null);
+	@Rule
+	public JenkinsRule jenkins = new JenkinsRule();
 
-	FreeStyleProject project = jenkins.createFreeStyleProject("CheckoutUnrestrictedView");
-	ManualWorkspaceImpl workspace = new ManualWorkspaceImpl("none", false, client, spec);
-        
-        Populate populate = new AutoCleanImpl(true, true, false, false, false, "", null);
-	PerforceScm scm = new PerforceScm(CREDENTIAL, workspace, populate);
-	project.setScm(scm);
-	project.save();
-        
-        // Request change 17 and expect change 17 as it's included in the workspace view
-        final String requestedChangelist = "17";
-        final String expectedChangelist = requestedChangelist;
+	@Rule
+	public SampleServerRule p4d = new SampleServerRule(P4ROOT, R15_1);
 
-	List<ParameterValue> list = new ArrayList<>();
-	list.add(new StringParameterValue(ReviewProp.SWARM_STATUS.toString(), "submitted"));
-	list.add(new StringParameterValue(ReviewProp.P4_CHANGE.toString(), requestedChangelist));
-	Action actions = new SafeParametersAction(new ArrayList<>(), list);
+	@Before
+	public void buildCredentials() throws Exception {
+		createCredentials("jenkins", "jenkins", p4d.getRshPort(), CREDENTIAL);
+	}
 
-	FreeStyleBuild build;
-	Cause.UserIdCause cause = new Cause.UserIdCause();
-	build = project.scheduleBuild2(0, cause, actions).get();
-	assertEquals(Result.SUCCESS, build.getResult());
+	@Test
+	public void testCheckoutUnrestrictedView() throws Exception {
+		String client = "CheckoutUnrestrictedView.ws";
+		String view = "//depot/... //" + client + "/...";
+		WorkspaceSpec spec = new WorkspaceSpec(view, null);
 
-        Map<String, String> env = new HashMap<>();
-        scm.buildEnvVars(build, env);
-        
-        // Assert that the workspace sync'd the expected change
-	assertEquals(env.get("P4_CHANGELIST"), expectedChangelist);
-    }
-    
-    @Issue("JENKINS-57534")
-    @Ignore 
-    @Test
-    public void testCheckoutRestrictedView() throws Exception {
-        String client = "CheckoutRestrictedView.ws";
-        String view = "//depot/Main/... //" + client + "/Main/...";
-	WorkspaceSpec spec = new WorkspaceSpec(view, null);
+		FreeStyleProject project = jenkins.createFreeStyleProject("CheckoutUnrestrictedView");
+		ManualWorkspaceImpl workspace = new ManualWorkspaceImpl("none", false, client, spec);
 
-	FreeStyleProject project = jenkins.createFreeStyleProject("CheckoutRestrictedView");
-	ManualWorkspaceImpl workspace = new ManualWorkspaceImpl("none", false, client, spec);
-        
-        Populate populate = new AutoCleanImpl(true, true, false, false, false, "", null);
-	PerforceScm scm = new PerforceScm(CREDENTIAL, workspace, populate);
-	project.setScm(scm);
-	project.save();
+		Populate populate = new AutoCleanImpl(true, true, false, false, false, "", null);
+		PerforceScm scm = new PerforceScm(CREDENTIAL, workspace, populate);
+		project.setScm(scm);
+		project.save();
 
-	// Request change 17, but expect change 16 as change 17 is not included in the workspace view
-        final String requestedChangelist = "17";
-        final String expectedChangelist = "16";
+		// Request change 17 and expect change 17 as it's included in the workspace view
+		final String requestedChangelist = "17";
+		final String expectedChangelist = requestedChangelist;
 
-	List<ParameterValue> list = new ArrayList<>();
-	list.add(new StringParameterValue(ReviewProp.SWARM_STATUS.toString(), "submitted"));
-	list.add(new StringParameterValue(ReviewProp.P4_CHANGE.toString(), requestedChangelist));
-	Action actions = new SafeParametersAction(new ArrayList<>(), list);
+		List<ParameterValue> list = new ArrayList<>();
+		list.add(new StringParameterValue(ReviewProp.SWARM_STATUS.toString(), "submitted"));
+		list.add(new StringParameterValue(ReviewProp.P4_CHANGE.toString(), requestedChangelist));
+		Action actions = new SafeParametersAction(new ArrayList<>(), list);
 
-	FreeStyleBuild build;
-	Cause.UserIdCause cause = new Cause.UserIdCause();
-	build = project.scheduleBuild2(0, cause, actions).get();
-	assertEquals(Result.SUCCESS, build.getResult());
+		FreeStyleBuild build;
+		Cause.UserIdCause cause = new Cause.UserIdCause();
+		build = project.scheduleBuild2(0, cause, actions).get();
+		assertEquals(Result.SUCCESS, build.getResult());
 
-        Map<String, String> env = new HashMap<>();
-        scm.buildEnvVars(build, env);
-        
-        // Assert that the workspace sync'd the expected change
-	assertEquals(env.get("P4_CHANGELIST"), expectedChangelist);
-    }
+		Map<String, String> env = new HashMap<>();
+		scm.buildEnvVars(build, env);
+
+		// Assert that the workspace sync'd the expected change
+		assertEquals(env.get("P4_CHANGELIST"), expectedChangelist);
+	}
+
+	@Issue("JENKINS-57534")
+	@Ignore
+	@Test
+	public void testCheckoutRestrictedView() throws Exception {
+		String client = "CheckoutRestrictedView.ws";
+		String view = "//depot/Main/... //" + client + "/Main/...";
+		WorkspaceSpec spec = new WorkspaceSpec(view, null);
+
+		FreeStyleProject project = jenkins.createFreeStyleProject("CheckoutRestrictedView");
+		ManualWorkspaceImpl workspace = new ManualWorkspaceImpl("none", false, client, spec);
+
+		Populate populate = new AutoCleanImpl(true, true, false, false, false, "", null);
+		PerforceScm scm = new PerforceScm(CREDENTIAL, workspace, populate);
+		project.setScm(scm);
+		project.save();
+
+		// Request change 17, but expect change 16 as change 17 is not included in the workspace view
+		final String requestedChangelist = "17";
+		final String expectedChangelist = "16";
+
+		List<ParameterValue> list = new ArrayList<>();
+		list.add(new StringParameterValue(ReviewProp.SWARM_STATUS.toString(), "submitted"));
+		list.add(new StringParameterValue(ReviewProp.P4_CHANGE.toString(), requestedChangelist));
+		Action actions = new SafeParametersAction(new ArrayList<>(), list);
+
+		FreeStyleBuild build;
+		Cause.UserIdCause cause = new Cause.UserIdCause();
+		build = project.scheduleBuild2(0, cause, actions).get();
+		assertEquals(Result.SUCCESS, build.getResult());
+
+		Map<String, String> env = new HashMap<>();
+		scm.buildEnvVars(build, env);
+
+		// Assert that the workspace sync'd the expected change
+		assertEquals(env.get("P4_CHANGELIST"), expectedChangelist);
+	}
 }
