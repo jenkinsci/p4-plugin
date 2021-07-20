a22e123221c4bfbaafe11bc58fb7ef900f38efb2
Daniel Jeznach <daniel.jeznach@imgtec.com>
2021-07-20 14:35:06 +0200

    Add support for P4_INCREMENTAL parameter
    It has effect only when "Polling per change" filter is active.
    When P4_INCREMENTAL environment variable exists and has string value "false",
    "Polling per change" filter is ignored and all changelists are built.
    If this parameter does not exist or has value "true", "Polling per
    change" filter works normally.
    This is to allow controlling of "Polling per change" behavior in
    FreeStyleProject jobs.

--- src/main/resources/org/jenkinsci/plugins/p4/build/P4EnvironmentContributor/buildEnv.groovy
+++ src/main/resources/org/jenkinsci/plugins/p4/build/P4EnvironmentContributor/buildEnv.groovy
@@ -2,7 +2,7 @@ package org.jenkinsci.plugins.p4.PerforceScm;
 
 def l = namespace(lib.JenkinsTagLib)
 
-["P4_CHANGELIST","P4_CLIENT","P4_PORT","P4_ROOT","P4_USER","P4_TICKET","P4_REVIEW","P4_REVIEW_TYPE","HUDSON_CHANGELOG_FILE","JENKINSFILE_PATH"].each { name ->
+["P4_CHANGELIST","P4_CLIENT","P4_PORT","P4_ROOT","P4_USER","P4_TICKET","P4_REVIEW","P4_REVIEW_TYPE","P4_INCREMENTAL","HUDSON_CHANGELOG_FILE","JENKINSFILE_PATH"].each { name ->
     l.buildEnvVar(name:name) {
         raw(_("${name}.blurb"))
     }
