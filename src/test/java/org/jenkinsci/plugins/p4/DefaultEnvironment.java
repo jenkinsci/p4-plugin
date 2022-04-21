49b2dda1d932a9b6e0937e6b65da9445f9ebbaa6
skumar <skumar@perforce.com>
2022-04-21 12:51:10 +0530

    JENKINS-64946 : Branch indexing occurring for every multibranch job execution
    WHAT
    
    1. If multibranch project trigger a build for a branch then it should
    build for head which is in the Observer and not for all heads that
    matches the multibranch.

--- src/test/java/org/jenkinsci/plugins/p4/DefaultEnvironment.java
+++ src/test/java/org/jenkinsci/plugins/p4/DefaultEnvironment.java
@@ -236,7 +236,7 @@ abstract public class DefaultEnvironment {
 	}
 
 	protected boolean waitForBuild(WorkflowJob job, int buildNumber) throws InterruptedException {
-		return waitForBuild(job, buildNumber, 50, 200L);
+		return waitForBuild(job, buildNumber, 170, 200L);
 	}
 
 	protected boolean waitForBuild(WorkflowJob job, int buildNumber, final int retry, long delay) throws InterruptedException {
