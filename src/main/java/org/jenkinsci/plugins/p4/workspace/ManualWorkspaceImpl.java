c984649599f429412de4a767eff8b9abaf9c475f
Paul Allen <pallen@perforce.com>
2022-12-20 10:26:06 +0000

    Remove self-closing element

--- src/main/java/org/jenkinsci/plugins/p4/workspace/ManualWorkspaceImpl.java
+++ src/main/java/org/jenkinsci/plugins/p4/workspace/ManualWorkspaceImpl.java
@@ -135,15 +135,14 @@ public class ManualWorkspaceImpl extends Workspace implements Serializable {
 
 	/**
 	 * Support for old plugin view mappings, see JENKINS-69491.
-	 * Two possible actions:<br/>
+	 * Two possible actions:
 	 * 1) If view has only a LHS, construct a RHS.
 	 * <pre>//depot/path/to/product/...</pre>
 	 * becomes:
 	 * <pre>//depot/path/to/product/... //CLIENT/path/to/product/...</pre>
-	 * Note that this is different from a p4sync constructed RHS.<br/>
+	 * Note that this is different from a p4sync constructed RHS.
 	 * 2) if view came from a file, substitute the clientName in the RHS.  p4sync has already
 	 * created the RHS correctly so don't alter.
-	 * <br/>
 	 * @param line       one line of a view
 	 * @param clientName client name for RHS
 	 * @param adjustClientName use true to fix clientName in RHS (for when RHS comes from a file)
