d64128167fe0a3bba1b485ad60da9882771160b5
skumar7322 <skumar@perforce.com>
2025-01-31 11:15:17 +0530

    Fix issues when simultaneously builds are running

--- src/main/java/org/jenkinsci/plugins/p4/review/ReviewNotifier.java
+++ src/main/java/org/jenkinsci/plugins/p4/review/ReviewNotifier.java
@@ -11,7 +11,6 @@ import hudson.model.listeners.RunListener;
 import hudson.scm.SCM;
 import jenkins.model.Jenkins;
 import jenkins.model.JenkinsLocationConfiguration;
-import jenkins.util.Timer;
 import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.plugins.p4.PerforceScm;
 import org.jenkinsci.plugins.p4.workflow.source.P4SwarmUpdateAction;
@@ -26,8 +25,8 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -35,21 +34,13 @@ import java.util.logging.Logger;
 public class ReviewNotifier extends RunListener<Run> {
 
 	private static final Logger logger = Logger.getLogger(ReviewNotifier.class.getName());
-	/*
-	 * URL parameters and data for callbacks
-	 */
-	private String failCallback;
-	private String passCallback;
-	private String updateCallback;
-	private String buildUrl = "";
-	private boolean isUpdateURLEnabled = true; // global config
+	private boolean isUpdateURLEnabled = true; // global config can be class variable
 
 	@Override
 	public void onCompleted(Run run, TaskListener listener) {
 		if (run instanceof MatrixRun) {
 			return;
 		}
-
 		logger.fine("ReviewNotifier: onCompleted");
 		try {
 			Result result = run.getResult();
@@ -57,18 +48,22 @@ public class ReviewNotifier extends RunListener<Run> {
 				logger.warning("Cannot notify onCompleted - job Result is null!");
 				return;
 			}
-			populateURLs(run, listener);
 
+			String buildURL = getBuildURL(run);
+			EnvVars env = run.getEnvironment(listener);
+			String updateCallback = env.get(ReviewProp.SWARM_UPDATE.getProp());
 			if (isUpdateURLEnabled && StringUtils.isNotEmpty(updateCallback)) {
 				String status = (result.equals(Result.SUCCESS)) ? "pass" : "fail";
 				List<P4SwarmUpdateAction> actions = run.getActions(P4SwarmUpdateAction.class);
-				if (!CollectionUtils.isEmpty(actions)) {
+				if (!CollectionUtils.isEmpty(actions) || result.equals(Result.FAILURE)) {
 					List<String> message = getUpdateMessage(actions);
-					notifySwarmUpdate(updateCallback, status, message);
+					notifySwarmUpdate(updateCallback, status, message, buildURL);
 				}
 			} else {
+				String failCallback = env.get(ReviewProp.SWARM_FAIL.getProp());
+				String passCallback = env.get(ReviewProp.SWARM_PASS.getProp());
 				String callbackURL = (result.equals(Result.SUCCESS)) ? passCallback : failCallback;
-				notifySwarmPassFail(callbackURL);
+				notifySwarmPassFail(callbackURL, buildURL);
 			}
 
 		} catch (Exception e) {
@@ -78,47 +73,54 @@ public class ReviewNotifier extends RunListener<Run> {
 
 	@Override
 	public void onStarted(Run run, TaskListener listener) {
-		if (run instanceof MatrixRun) {
+		if (run == null || run instanceof MatrixRun) {
 			return;
 		}
 		logger.fine("ReviewNotifier: onStarted");
+		Jenkins j = Jenkins.getInstanceOrNull();
+		if (j == null) {
+			return;
+		}
+		Descriptor<SCM> scm = j.getDescriptor(PerforceScm.class);
+		PerforceScm.DescriptorImpl p4scm = (PerforceScm.DescriptorImpl) scm;
+		if (p4scm != null) {
+			isUpdateURLEnabled = p4scm.isUseSwarmUpdateUrl();
+		}
+
 		try {
-			populateURLs(run, listener);
+			EnvVars env = run.getEnvironment(listener);
+			String updateCallback = env.get(ReviewProp.SWARM_UPDATE.getProp());
 			if (isUpdateURLEnabled && StringUtils.isNotEmpty(updateCallback)) {
 				// onStarted() is only called once when build is stated. A scheduled task required to get message and notify update periodically
-				scheduleSwarmUpdateURL(run);
+				Timer timer = new Timer();
+
+				TimerTask task = new TimerTask() {
+					@Override
+					public void run() {
+						if (run.isBuilding()) {
+							List<P4SwarmUpdateAction> actions = run.getActions(P4SwarmUpdateAction.class);
+							if (!CollectionUtils.isEmpty(actions)) {
+								List<String> message = getUpdateMessage(actions);
+								try {
+									String buildURL = getBuildURL(run);
+									notifySwarmUpdate(updateCallback, "running", message, buildURL);
+								} catch (Exception e) {
+									logger.log(Level.WARNING, "Unable to notify swarm update: " + e.getMessage(), e);
+								}
+							}
+						} else {
+							logger.log(Level.INFO, "Build finished: " + run.getFullDisplayName());
+							timer.cancel();
+						}
+					}
+				};
+				timer.scheduleAtFixedRate(task, 2000, 10000);
 			} else {
 				logger.log(Level.INFO, "Skipping job onStarted because use update url is disabled");
 			}
 		} catch (Exception e) {
 			logger.log(Level.INFO, "onStarted Unable to Notify Review: " + e.getMessage(), e);
 		}
-	}
-
-	private void scheduleSwarmUpdateURL(Run run) {
-		if (run == null) {
-			return;
-		}
-		TimerTask task = new TimerTask() {
-			@Override
-			public void run() {
-				if (run.isBuilding()) {
-					List<P4SwarmUpdateAction> actions = run.getActions(P4SwarmUpdateAction.class);
-					if (!CollectionUtils.isEmpty(actions)) {
-						List<String> message = getUpdateMessage(actions);
-						try {
-							notifySwarmUpdate(updateCallback, "running", message);
-						} catch (Exception e) {
-							logger.log(Level.WARNING, "Unable to Notify Review: " + e.getMessage(), e);
-						}
-					}
-				} else {
-					logger.log(Level.INFO, "Build finished: " + run.getFullDisplayName());
-					this.cancel();
-				}
-			}
-		};
-		Timer.get().scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
 	}
 
 	private static List<String> getUpdateMessage(List<P4SwarmUpdateAction> action) {
@@ -139,7 +141,7 @@ public class ReviewNotifier extends RunListener<Run> {
 	 * @param status   Swarm supported status values are running, fail, pass.  Not enforced.
 	 * @param messages zero or more messages to send
 	 */
-	private void notifySwarmUpdate(String callback, String status, List<String> messages) throws Exception {
+	private void notifySwarmUpdate(String callback, String status, List<String> messages, String buildUrl) throws Exception {
 		logger.info("ReviewNotifier set status=" + status + " to " + callback);
 		if (StringUtils.isEmpty(callback)) {
 			return;
@@ -155,12 +157,12 @@ public class ReviewNotifier extends RunListener<Run> {
 		logger.info("Response code: " + http.getResponseCode());
 	}
 
-	private void notifySwarmPassFail(String callback) throws Exception {
-		logger.info("ReviewNotifier to " + callback + " with url=" + this.buildUrl);
+	private void notifySwarmPassFail(String callback, String buildUrl) throws Exception {
+		logger.info("ReviewNotifier to " + callback + " with url=" + buildUrl);
 		if (StringUtils.isEmpty(callback)) {
 			return;
 		}
-		String postContent = "url=" + this.buildUrl;
+		String postContent = "url=" + buildUrl;
 		HttpURLConnection http = postRequest(callback, postContent);
 		int response = http.getResponseCode();
 		logger.info("Response code: " + response);
@@ -187,30 +189,20 @@ public class ReviewNotifier extends RunListener<Run> {
 	 * @param listener
 	 * @throws Exception
 	 */
-	private void populateURLs(Run run, TaskListener listener) throws Exception {
-		EnvVars env = run.getEnvironment(listener);
-		failCallback = env.get(ReviewProp.SWARM_FAIL.getProp());
-		passCallback = env.get(ReviewProp.SWARM_PASS.getProp());
-		updateCallback = env.get(ReviewProp.SWARM_UPDATE.getProp());
-
+	private String getBuildURL(Run run) throws Exception {
 		Jenkins j = Jenkins.getInstanceOrNull();
 		if (j == null) {
 			// should never be here.
 			logger.warning("Internal failure onCompleted:  no jenkins instance, assuming useUpdateUrl=" + isUpdateURLEnabled);
+			return "";
 		} else {
 			String rootUrl = j.getRootUrl();
 			if (rootUrl == null || rootUrl.isEmpty()) {
 				JenkinsLocationConfiguration globalConfig = new JenkinsLocationConfiguration();
 				rootUrl = (globalConfig.getUrl() == null) ? "missing Jenkins URL in global config" : globalConfig.getUrl();
 			}
-			buildUrl = rootUrl + run.getUrl();
-
-			Descriptor<SCM> scm = j.getDescriptor(PerforceScm.class);
-			PerforceScm.DescriptorImpl p4scm = (PerforceScm.DescriptorImpl) scm;
-			if (p4scm != null) {
-				isUpdateURLEnabled = p4scm.isUseSwarmUpdateUrl();
-			}
+			return rootUrl + run.getUrl();
 		}
-	}
 
+	}
 }
