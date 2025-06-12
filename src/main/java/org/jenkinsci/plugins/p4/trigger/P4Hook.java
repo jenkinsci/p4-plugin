d4d643aeb71995f1e4b096d4bad078c057a0e3e7
stuartrowe <stuartrowe@gmail.com>
2025-06-12 11:12:06 -0700

    Change P4Hook symbol to P4 (product) Logo

--- src/main/java/org/jenkinsci/plugins/p4/trigger/P4Hook.java
+++ src/main/java/org/jenkinsci/plugins/p4/trigger/P4Hook.java
@@ -37,7 +37,7 @@ public class P4Hook implements UnprotectedRootAction {
 
 	@Override
 	public String getIconFileName() {
-		return "symbol-perforce plugin-p4";
+		return "symbol-logo-p4-icon plugin-p4";
 	}
 
 	@Override
