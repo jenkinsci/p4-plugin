90347c4cf6a5cac4abc222de4cd51c8a0bf32ebc
ajindal-mdx <ajindal@perforce.com>
2025-08-12 11:23:41 +0530

    Fixing the Variable Description visibility

--- src/main/resources/org/jenkinsci/plugins/p4/PerforceScm/buildEnv.groovy
+++
@@ -1,9 +0,0 @@
-package org.jenkinsci.plugins.p4.PerforceScm;
-
-def l = namespace(lib.JenkinsTagLib)
-
-["P4_CHANGELIST","P4_CLIENT","P4_PORT","P4_ROOT","P4_USER","P4_TICKET","P4_REVIEW","P4_REVIEW_TYPE","P4_INCREMENTAL","P4_CLEANWORKSPACE","HUDSON_CHANGELOG_FILE","JENKINSFILE_PATH"].each { name ->
-    l.buildEnvVar(name:name) {
-        raw(_("${name}.blurb"))
-    }
-}
--- src/main/resources/org/jenkinsci/plugins/p4/build/P4EnvironmentContributor/buildEnv.groovy
+++ src/main/resources/org/jenkinsci/plugins/p4/build/P4EnvironmentContributor/buildEnv.groovy
@@ -1,9 +1,24 @@
 package org.jenkinsci.plugins.p4.PerforceScm;
 
+def stream = getClass().getResourceAsStream("buildEnv.properties")
 def l = namespace(lib.JenkinsTagLib)
+def orderedProps = new LinkedHashMap()
 
-["P4_CHANGELIST","P4_CLIENT","P4_PORT","P4_ROOT","P4_USER","P4_TICKET","P4_REVIEW","P4_REVIEW_TYPE","P4_INCREMENTAL","P4_CLEANWORKSPACE","HUDSON_CHANGELOG_FILE","JENKINSFILE_PATH","P4_STREAM_AT_CHANGE"].each { name ->
-    l.buildEnvVar(name:name) {
-        raw(_("${name}.blurb"))
+if (stream != null) {
+    stream.eachLine { line ->
+        line = line.trim()
+        if (!line || line.startsWith("#")) return  // Skip empty or commented lines
+        def parts = line.split("=", 2)
+        if (parts.length == 2 && parts[0].endsWith(".blurb")) {
+            orderedProps.put(parts[0], parts[1])
+            //orderedProps[parts[0]] = parts[1]
+        }
+    }
+
+    orderedProps.each { key, value ->
+        def name = key.replace(".blurb", "")
+        l.buildEnvVar(name: name) {
+            raw(value ?: "No description available")
+        }
     }
 }
