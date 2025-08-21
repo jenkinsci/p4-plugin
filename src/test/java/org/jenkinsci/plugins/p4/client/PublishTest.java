529d653db61a45a06d1b9641aaa2baa270b64cb4
ajindal-mdx <ajindal@perforce.com>
2025-08-21 12:10:46 +0530

    Fixing the testcases

--- src/test/java/org/jenkinsci/plugins/p4/client/PublishTest.java
+++ src/test/java/org/jenkinsci/plugins/p4/client/PublishTest.java
@@ -198,7 +198,7 @@ public class PublishTest extends DefaultEnvironment {
 				+ "      name: 'jenkins-${NODE_NAME}-${JOB_NAME}',\n"
 				+ "      spec: [view: '//depot/... //jenkins-${NODE_NAME}-${JOB_NAME}/...']]\n"
 				+ "   def syncOptions = [$class: 'org.jenkinsci.plugins.p4.populate.SyncOnlyImpl',\n"
-				+ "      revert:true, have:true, modtime:true]\n"
+				+ "      revert:true, have:true]\n"
 				+ "   p4sync workspace:workspace, credential: '" + CREDENTIAL + "', populate: syncOptions\n"
 				+ "}", false));
 		WorkflowRun run = jenkins.assertBuildStatusSuccess(job.scheduleBuild2(0));
